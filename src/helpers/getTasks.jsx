d8d6030814277d306d0d92118e1669af1d061879
AbderrSfa <abderrahmanesfaihi@hotmail.com>
2022-10-08 16:34:19 +0100

    Made the api using next.js

--- src/helpers/getTasks.jsx
+++ src/helpers/getTasks.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getTasks = async () => {
 	try {
-		const res = await axios.get('http://localhost:4040/api/todos');
+		const res = await axios.get('http://localhost:3000/api/todos');
 		return res.data;
 	} catch (error) {
 		console.log(error);
